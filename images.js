aaeac8a0b26c8c7ae75acb40485a87aa562ec1d5
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-11-26 15:33:18 -0800

    Extracted image data into json structure. Fetching it, and building out the carousel with js

---
+++ images.js
@@ -0,0 +1,82 @@
+[
+  {
+    "id": 1,
+    "alt": "A sunrise above a cloud inversion amongst jagged rocky and snowy mountains",
+    "imgSrc": "assets/clouds-mobile-480.jpg",
+    "imgSrcset": "assets/clouds-mobile-480.jpg 480w, assets/clouds-mobile-900.jpg 900w",
+    "sources": [
+      {
+        "media": "(min-width: 1200px)",
+        "srcset": "assets/clouds-desktop-1600.jpg 1600w, assets/clouds-desktop-2000.jpg 2000w, assets/clouds-desktop-2400.jpg 2400w"
+      },
+      {
+        "media": "(min-width: 1000px)",
+        "srcset": "assets/clouds-laptop-1200.jpg 1200w, assets/clouds-laptop-1600.jpg 1600w"
+      },
+      {
+        "media": "(min-width: 600px)",
+        "srcset": "assets/clouds-tablet-800.jpg 800w, assets/clouds-tablet-1200.jpg 1200w"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "alt": "A sunrise amongst the mountains with dark shadows and red lighting",
+    "imgSrc": "assets/dark-mobile-480.jpg",
+    "imgSrcset": "assets/dark-mobile-480.jpg 480w, assets/dark-mobile-900.jpg 900w",
+    "sources": [
+      {
+        "media": "(min-width: 1200px)",
+        "srcset": "assets/dark-desktop-1600.jpg 1600w, assets/dark-desktop-2000.jpg 2000w, assets/dark-desktop-2400.jpg 2400w"
+      },
+      {
+        "media": "(min-width: 1000px)",
+        "srcset": "assets/dark-laptop-1200.jpg 1200w, assets/dark-laptop-1600.jpg 1600w"
+      },
+      {
+        "media": "(min-width: 600px)",
+        "srcset": "assets/dark-tablet-800.jpg 800w, assets/dark-tablet-1200.jpg 1200w"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "alt": "People in wooden rowboats on a turquoise lake, nestled beneath towering rocky mountains, underneath a bright blue sunny sky",
+    "imgSrc": "assets/lake-mobile-480.jpg",
+    "imgSrcset": "assets/lake-mobile-480.jpg 480w, assets/lake-mobile-900.jpg 900w",
+    "sources": [
+      {
+        "media": "(min-width: 1200px)",
+        "srcset": "assets/lake-desktop-1600.jpg 1600w, assets/lake-desktop-2000.jpg 2000w, assets/lake-desktop-2400.jpg 2400w"
+      },
+      {
+        "media": "(min-width: 1000px)",
+        "srcset": "assets/lake-laptop-1200.jpg 1200w, assets/lake-laptop-1600.jpg 1600w"
+      },
+      {
+        "media": "(min-width: 600px)",
+        "srcset": "assets/lake-tablet-800.jpg 800w, assets/lake-tablet-1200.jpg 1200w"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "alt": "A verdant desert canyon with rugged red walls",
+    "imgSrc": "assets/red-canyon-mobile-480.jpg",
+    "imgSrcset": "assets/red-canyon-mobile-480.jpg 480w, assets/red-canyon-mobile-900.jpg 900w",
+    "sources": [
+      {
+        "media": "(min-width: 1200px)",
+        "srcset": "assets/red-canyon-desktop-1600.jpg 1600w, assets/red-canyon-desktop-2000.jpg 2000w, assets/red-canyon-desktop-2400.jpg 2400w"
+      },
+      {
+        "media": "(min-width: 1000px)",
+        "srcset": "assets/red-canyon-laptop-1200.jpg 1200w, assets/red-canyon-laptop-1600.jpg 1600w"
+      },
+      {
+        "media": "(min-width: 600px)",
+        "srcset": "assets/red-canyon-tablet-800.jpg 800w, assets/red-canyon-tablet-1200.jpg 1200w"
+      }
+    ]
+  }
+]
--- index.js
+++ index.js
@@ -1,60 +1,124 @@
-let currentSlide = 1;
-const slides = document.getElementsByClassName('carousel__slide');
-const length = slides.length;
-const carousel__track = document.querySelector('.carousel__track');
-const button_previous = document.querySelector('.carousel__previous');
-const button_next = document.querySelector('.carousel__next');
-const dots = document.querySelectorAll('.carousel__dots button');
+document.addEventListener('DOMContentLoaded', carousel);
 
-carousel__track.style.width = `${length * 100}%`;
-
-button_previous.addEventListener('click', previous);
-button_next.addEventListener('click', next);
-
-dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        jumpToSlide(index + 1);
+function carousel() {
+    const carousels = document.querySelectorAll('.carousel');
+    carousels.forEach((carouselWrapper) => {
+        createCarousel(carouselWrapper);
     });
-    dot.setAttribute('data-slide', index);
-});
-for(const slide of slides) {
-    slide.style.width = 100/length + '%';
 }
 
-udpateDots();
+async function createCarousel(carouselWrapper) {
+    const imagesDataSrc = carouselWrapper.getAttribute('data-images');
+    const imagesData = await getImagesData(imagesDataSrc);
+    
+    const length = imagesData.length;
+    let currentSlide = 1;
 
-function next() {
-    if(currentSlide === length) {
-        currentSlide = 1;
-    } else {
-        currentSlide++;
+    let track = document.createElement('div');
+    track.classList.add('carousel__track');
+    track.style.width = `${length * 100}%`;
+
+    for (const image of imagesData) {
+        const slide = document.createElement('div');
+        slide.classList.add('carousel__slide');
+        slide.style.width = 100/length + '%';
+
+        const picture = document.createElement('picture');
+
+        for(const source of image.sources) {
+            const sourceElement = document.createElement('source');
+            sourceElement.media = source.media;
+            sourceElement.srcset = source.srcset;
+            picture.appendChild(sourceElement);
+        }
+
+        const imgElement = document.createElement('img');
+        imgElement.src = image.imgSrc;
+        imgElement.srcset = image.imgSrcset;
+        imgElement.alt = image.alt;
+        imgElement.loading = "lazy";
+
+        picture.appendChild(imgElement);
+        slide.appendChild(picture);
+        track.appendChild(slide);
     }
-    updateCarouselTrackPosition();
-}
 
-function previous() {
-    if(currentSlide === 1) {
-        currentSlide = length;
-    } else {
-        currentSlide--;
+    carouselWrapper.appendChild(track);
+
+    const previousButton = document.createElement('button');
+    previousButton.classList.add('carousel__previous');
+    previousButton.addEventListener('click', previous);
+    carouselWrapper.appendChild(previousButton);
+
+    const nextButton = document.createElement('button');
+    nextButton.classList.add('carousel__next');
+    nextButton.addEventListener('click', next);
+    carouselWrapper.appendChild(nextButton);
+
+    const dots = document.createElement('div');
+    dots.classList.add('carousel__dots');
+    
+    for(let i = 1; i <= length; i++) {
+        const dot = document.createElement('button');
+        dot.setAttribute('data-slide', i);
+        dot.addEventListener('click', () => {
+            jumpToSlide(i);
+        })
+        dots.appendChild(dot);
     }
-    updateCarouselTrackPosition();
-}
+    
+    carouselWrapper.appendChild(dots);
 
-function updateCarouselTrackPosition() {
-    carousel__track.style.transform = `translateX(${-100/length * (currentSlide - 1)}%)`;
     udpateDots();
+
+    function next() {
+        if(currentSlide === length) {
+            currentSlide = 1;
+        } else {
+            currentSlide++;
+        }
+        updateCarouselTrackPosition();
+    }
+
+    function previous() {
+        if(currentSlide === 1) {
+            currentSlide = length;
+        } else {
+            currentSlide--;
+        }
+        updateCarouselTrackPosition();
+    }
+
+    function updateCarouselTrackPosition() {
+        track.style.transform = `translateX(${-100/length * (currentSlide - 1)}%)`;
+        udpateDots();
+    }
+
+    function udpateDots() {
+        for (const dot of dots.children) {
+            dot.classList.remove('carousel__dots--active');
+        }
+        dots.children[currentSlide - 1].classList.add('carousel__dots--active');
+    }
+
+    function jumpToSlide(slide) {
+        currentSlide = slide;
+        updateCarouselTrackPosition();
+    }
+
 }
 
-function udpateDots() {
-    dots.forEach((dot) => {
-        dot.classList.remove('carousel__dots--active');
-    });
-    dots[currentSlide - 1].classList.add('carousel__dots--active');
-}
-
-function jumpToSlide(slide) {
-    currentSlide = slide;
-    updateCarouselTrackPosition();
+async function getImagesData(src) {
+    try { 
+        const response = await fetch(src);
+        if(!response.ok) {
+            throw new Error("Could not fetch data");
+        }
+        const imagesData = await response.json();
+        return imagesData;
+    } catch(error) {
+        console.error(error);
+        return [];
+    }
 }
 
